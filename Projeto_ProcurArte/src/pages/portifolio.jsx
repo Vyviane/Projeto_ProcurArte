52c12191c803573d7cfcec4838103c6013fe2663
Vyviane <vyviane.ssantos@senapsp.edu.br>
2023-11-23 16:47:19 -0300

    Ajustes na pagina de portfolio

--- Projeto_ProcurArte/src/pages/portifolio.jsx
+++ Projeto_ProcurArte/src/pages/portifolio.jsx
@@ -1,10 +1,13 @@
 import "../styles/portifolio.scss"
 import Textarea from "../components/Textarea/Textarea"
-import P from "../components/P"
+import Perfil from "../components/Perfil"
+import Navbar from "../components/Navbar"
 
 export default function Portifolio() {
     return (
-        <div className="portifolio">
+        <div className="containerP">
+                 <Navbar/>
+            <div className="portifolio">
             <div className="inputs-portifolio">
                 <div className="inputs-2">
                     <div className="select">
@@ -72,39 +75,8 @@ export default function Portifolio() {
                 </div>
 
             </div>
-            <div className="exibir-portifolio">
-                <div className="first-s">
-                    <img src="../../Imagens/imageSergio.svg" className="img-perfil"/>
-                    <h2>Sergio mauro</h2>
-                </div>
-                <div className="second-s">
-                    <P
-                        name="estilo"
-                        text="Música classica"
-                        label="Estilo: "
-                    />
-                    <P
-                        name="instrumento"
-                        text="Violino"
-                        label="Instrumento: "
-                    />
-                    <P
-                        name="instagram"
-                        text="@sergio_do_mal"
-                        label="Instagram: "
-                    />
-                    <P
-                        name="eventos"
-                        text="Já trabalhei em festas, em orquestras, na rua..."
-                        label="Quais eventos você costuma se apresentar?: "
-                    />
-                    <P
-                        name="resumo"
-                        text="Toco instrumento profissionalmente desde os 12 anos..."
-                        label="Resumo profissional: "
-                    />
-                </div>
-            </div>
+            <Perfil/>
+        </div>
         </div>
     )
 }
